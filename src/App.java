fa2d4e4efea9e2e3f9ed63562a6f649ac1deb917
BrunoAmadei <brunoamadei98@gmail.com>
2023-11-14 13:31:38 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        //<tipo> identificador = valor;
+        String saudacao = "Olá, bixo feio";
+        System.out.println(saudacao);
+       
     }
 }
