157d2e0cf39b3e85954f3f00fb5150fd3f7c68bd
BrunoAmadei <brunoamadei98@gmail.com>
2023-11-15 10:39:01 -0300

    Create Metodos.java

---
+++ src/Metodos.java
@@ -0,0 +1,15 @@
+public class Metodos {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+     * <tipo retorno do metodo> identificador(<tipo> parametros){
+     * <bloco de comandos>
+     * }
+     */
+    void ativarSoneca() {
+        System.out.println("Soneca ativada");
+    }
+
+}
